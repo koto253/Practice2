223aa7a8c7565448dfea979b72938146d2bb26ca
Ismael Bah <90379217+koto253@users.noreply.github.com>
2022-03-10 18:28:47 -0500

    Update login.php

--- login.php
+++ login.php
@@ -13,14 +13,33 @@ $pass = $_POST["Password"];
 $pass2 = addslashes($_POST["Password"]);
 $passLen = strlen($_POST["Password"]);
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Login Information</h1>
+    <p>Your username is <?php echo $name; ?></p>
+</body>
+
+</html>
+
 // Display
 echo "Original Username: $name <br>
-      Original Password: $pass <br>";
+Original Password: $pass <br>";
 echo trim("Modified username: $name2 <br>
-           Modified password: $pass2");
+Modified password: $pass2");
 
 echo md5($pass);
 
-// RETURN THE LENGTH OF THE STRING 
+// RETURN THE LENGTH OF THE STRING
 
 echo ("<br> Length password: $passLen");
