7a8043784374efe02baada31bd37da625c40096b
Ismael Bah <90379217+koto253@users.noreply.github.com>
2022-03-22 16:04:34 -0400

    update

--- calculate.php
+++ calculate.php
@@ -5,16 +5,79 @@
     Ismael Bah
 */
 // Retrieve form $_GET autoglobal array
-$secs = $_GET["Seconds"];
+$totalSecond = $_GET["Seconds"];
 
-// calculate the amount of hour and second
-$hour = floor($secs / 3600);
-$remainSec = $secs % 3600;
-$min = floor($remainSec / 60);
-$second = $remainSec % 60;
+// calculate the amount of hours, minutes, and seconds
+$hours = floor($totalSecond / 3600);
+$remainSec = $totalSecond % 3600;
+$minutes = floor($remainSec / 60);
+$seconds = $remainSec % 60;
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-echo "<p>
-$hour hour, $min min and $second seconds
-</p>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: antiquewhite;
+            align-items: center;
+            font-family: monospace;
+            margin: 100px;
+            font-size: 18px;
+            padding: 15px 0 15px 5px;
+        }
+
+        form {
+            display: inline;
+        }
+
+        .content {
+            margin: auto;
+            max-width: 375px;
+            border-radius: 18px;
+        }
+
+        input {
+            border: none;
+        }
+
+        .submit {
+            background-color: blueviolet;
+            text-align: center;
+            padding: 5px;
+            margin-top: 10px;
+
+        }
+
+        p {
+            padding-top: 5px;
+        }
+
+        fieldset {
+            padding: 20px;
+            background-color: aquamarine;
+            box-shadow: 2px;
+            border: none;
+
+        }
+    </style>
+</head>
+
+<body>
+    <h1></h1>
+    <p> <?php echo "{$hours}hr(s), {$minutes}min(s) and {$seconds}sec(s)" ?> </p>
+
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -5,25 +5,89 @@
     Ismael Bah
 */
 
-//Variables 
+// Retrieve forn values
 $name = $_POST["Username"];
 $pass = $_POST["Password"];
 
-// Variable with addslashes
-$modName = addslashes($_POST["Username"]);
-$modPass = addslashes($_POST["Password"]);
-$passLen = strlen($_POST["Password"]);
+// Variable with PHP STRING FUNCTIONs
+$modName = addslashes($name);
+$modPass = addslashes($pass);
+$trimUsername = trim($name);
+$passLen = strlen($pass);
+$md5Password = md5($pass);
 
-// Display
-echo "Original Username: $name <br>
-      Original Password: $pass <br>";
 
-// Display with trim()
-echo trim("Modified username: $modName <br>
-           Modified password: $modPass");
 
-echo md5($pass);
+?>
 
-// RETURN THE LENGTH OF THE STRING
+<!DOCTYPE html>
+<html lang="en">
 
-echo ("<br> Length_Of_Password: $passLen");
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: antiquewhite;
+            align-items: center;
+            font-family: monospace;
+            margin: 100px;
+            font-size: 18px;
+            padding: 15px 0 15px 5px;
+        }
+
+        form {
+            display: inline;
+        }
+
+        .content {
+            margin: auto;
+            max-width: 375px;
+            border-radius: 18px;
+        }
+
+        input {
+            border: none;
+        }
+
+        .submit {
+            background-color: blueviolet;
+            text-align: center;
+            padding: 5px;
+            margin-top: 10px;
+
+        }
+
+        p {
+            padding-top: 5px;
+        }
+
+        fieldset {
+            padding: 20px;
+            background-color: aquamarine;
+            box-shadow: 2px;
+            border: none;
+
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Login Information</h1>
+    <pre> Your username is <?php echo $name ?> </pre>
+    <pre> Modified username is <?php echo $modName ?> </pre>
+    <pre> Trimmed username is <?php echo $trimUsername ?> </pre>
+    <pre> Your password is <?php echo $pass ?> </pre>
+    <pre> Modified password is <?php echo $modPass ?> </pre>
+    <pre> Length Of Password is <?php echo $passLen ?> </pre>
+    <pre> md5Password is <?php echo $md5Password ?> </pre>
+</body>
+
+</html>
